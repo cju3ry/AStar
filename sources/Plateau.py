005c46e329da501a2852f21b5ceddb9ee01166f6
qchesnier <168072017+qchesnier@users.noreply.github.com>
2025-01-29 18:38:18 +0100

    generation de plateau

--- sources/Plateau.py
+++ sources/Plateau.py
@@ -17,12 +17,30 @@ class Plateau:
 def getListeCases(self) :
     return self.listeCases
 
-def genererPlateau(longueur, largeur) :
-    if (longueur > 2 | largeur > 2 | type(longueur) != int | type(largeur) != int) :
+def genererPlateau(longueur, largeur, tauxDeMur, departArriveeOk : bool) :
+    if longueur > 2 | largeur > 2 | type(longueur) != int | type(largeur) != int :
         print("la longueur et la largeur doivent être des entiers supérieur ou égal à 3")
 
-    if (longueur == None & largeur == None) :
+    if longueur == None & largeur == None :
         longueur = random.randint(3,20)
         largeur = random.randint(3,20)
-    
-    for 
+
+    taillePlateau = longueur * largeur
+
+    if not departArriveeOk :
+        emplacementDepart = random.randint(0, taillePlateau)
+        emplacementArrivee = random.randint(0, taillePlateau)
+        
+    chaineEtat = ""
+    for i in range(taillePlateau) :
+        if (departArriveeOk & i == 0) :
+            chaineEtat = 'D'
+        elif (departArriveeOk & taillePlateau == i) :
+            chaineEtat = chaineEtat + 'A'
+        elif (emplacementDepart == i) :
+            chaineEtat = chaineEtat + 'D'
+        elif (emplacementArrivee == i) :
+            chaineEtat = chaineEtat + 'A'
+        else :
+            generationMur = random.randint(tauxDeMur * 10, 10)
+            if () :
