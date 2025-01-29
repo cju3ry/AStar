40cdf99bfd0fa3bccdf2b9c6167db1fafc5d40ce
qchesnier <168072017+qchesnier@users.noreply.github.com>
2025-01-29 11:31:12 +0100

    commencement genererPlateau

--- sources/Plateau.py
+++ sources/Plateau.py
@@ -1,4 +1,5 @@
-import Case from Case
+from Case import Case
+import random
 class Plateau:
     def __init__(self, longueur, largeur, listeCases):
         self.longueur = longueur
@@ -8,9 +9,20 @@ class Plateau:
             print ("erreur")
 
         for case in listeCases :
-            if (case.getX() > self.longueur | case.getY() > self.largeur) :
+            if (case.get_x() > self.longueur | case.get_y() > self.largeur) :
                 print("erreur")
         
         self.listeCases = listeCases
 
+def getListeCases(self) :
+    return self.listeCases
+
+def genererPlateau(longueur, largeur) :
+    if (longueur > 2 | largeur > 2 | type(longueur) != int | type(largeur) != int) :
+        print("la longueur et la largeur doivent être des entiers supérieur ou égal à 3")
+
+    if (longueur == None & largeur == None) :
+        longueur = random.randint(3,20)
+        largeur = random.randint(3,20)
     
+    for 
