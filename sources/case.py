f26077948456bb28bc5fcfbf19cb0d251d0afb66
cju3ry <clement.juery@iut-rodez.fr>
2025-01-29 08:57:15 +0100

    modification du nom de la classe point en case et moduf du constructeur

---
+++ sources/case.py
@@ -0,0 +1,45 @@
+class Case:
+    def __init__(self, x, y, char):
+        self.x = x
+        self.y = y
+        self.g = 0
+        self.h = 0
+        self.f = 0
+        if char not in {"O", "X", "D", "A"}:
+                raise ValueError(f"Caractère non conforme : {char}")
+        self.char = char
+
+
+    def calcul_heuristique(self, arrive):
+        self.h = abs(self.x - arrive.x) + abs(self.y - arrive.y)
+
+    def calcul_f(self):
+        self.f = self.g + self.h
+
+    def set_g(self, valeur_g):
+        self.g = valeur_g
+        self.calcul_f()
+
+    def __repr__(self):
+        """
+        Représentation textuelle du case pour le débogage.
+        """
+        return (f"(x={self.x}, y={self.y}, g={self.g}, h={self.h}, f={self.f}, "
+                f"char={self.char},)")
+
+# Exemple d'utilisation
+if __name__ == "__main__":
+    start = Case(0, 0,"D")
+    end = Case(5, 5,"A")
+    wall = Case(2, 2, "X")
+
+    # Définir des coûts pour un case
+    case = Case(1, 1, "O")
+    case.set_g(10)  # Exemple de coût pour arriver à ce case
+    case.calcul_heuristique(end)  # Calcul de l'heuristique
+    case.calcul_f()  # Mise à jour du coût total
+
+    print(start)
+    print(end)
+    print(wall)
+    print(case)
