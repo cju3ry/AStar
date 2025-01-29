593e438dc0bcb9d1ed011fce5b814babe5a44059
cju3ry <clement.juery@iut-rodez.fr>
2025-01-29 10:54:24 +0100

    modification du constructeur pour ne pas voir des negétifs dans les coordonées

--- sources/case.py
+++ sources/case.py
@@ -1,14 +1,17 @@
 class Case:
     def __init__(self, x, y, char):
+        if x < 0 or y < 0:
+            raise ValueError("Les valeurs des coordonnées (x et y) ne doivent pas être négatives !")
+
+        if char not in {"O", "X", "D", "A"}:
+            raise ValueError(f"Caractère non conforme : {char}. Attendu : 'O', 'X', 'D' ou 'A'.")
+
         self.x = x
         self.y = y
         self.g = 0
         self.h = 0
         self.f = 0
-        if char not in {"O", "X", "D", "A"}:
-                raise ValueError(f"Caractère non conforme : {char}")
         self.char = char
-
 
     def calcul_heuristique(self, arrive):
         self.h = abs(self.x - arrive.x) + abs(self.y - arrive.y)
