8c58d52103d78fbd8c9e5abd13e2857ef19afe0a
cju3ry <clement.juery@iut-rodez.fr>
2025-02-01 15:42:28 +0100

    correction bug et ajout mmethode de début

--- sources/AStar.py
+++ sources/AStar.py
@@ -2,10 +2,16 @@ from Case import Case
 from Plateau import Plateau
 
 def trouverFin(listeCase):
-    global caseFin
     for case in listeCase:
-        if(case.char == "A"):
+        if case.char == "A":
             caseFin = case
+    return caseFin
+
+def trouverDebut(listeCase):
+    for case in listeCase:
+        if case.char == "D":
+            caseDebut = case
+    return caseDebut
 
 def majListeAdjacente(caseActuelle, listeAdjacente, listeCase, Plateau):
     xActuel = caseActuelle.get_x()
@@ -38,10 +44,11 @@ def choixSuivant(caseActuelle, listeAdjacente, listeCase, Plateau, listeOuverte,
 
     if caseActuelle == caseFin:
         return chemin
+
     listeAdjacente = majListeAdjacente(caseActuelle, listeAdjacente, listeCase, Plateau)
     for adjacent in listeAdjacente:
         if adjacent not in listeFerme:
-            adjacent.set_g(len(listeOuverte))
+            adjacent.set_g(len(chemin))
             adjacent.calcul_heuristique(caseFin)
             adjacent.calcul_f()
             adjacent.set_predecesseur(caseActuelle)
